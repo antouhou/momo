22de96e5ddf8e6f0cd782fc8385deddee18d4e1b
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2026-06-11 18:03:46 -0300

    fix main menu style

--- crates/momo-ui/src/components/quick_settings/panel/style.rs
+++ crates/momo-ui/src/components/quick_settings/panel/style.rs
@@ -26,8 +26,9 @@ pub(crate) fn settings_menu_style(max_height: f32) -> Style {
 pub(crate) fn settings_content_style() -> Style {
     Style::new()
         .with_size_constraint(
-            SizeConstraint::exact_content_height().with_exact_width(SETTINGS_MENU_CONTENT_WIDTH),
+            SizeConstraint::fixed_width(SETTINGS_MENU_CONTENT_WIDTH),
         )
+        .with_grow(1.0)
         .with_direction(FlexDirection::Column)
         .with_spacing((SETTINGS_MENU_GAP, SETTINGS_MENU_GAP))
 }

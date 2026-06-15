2633b76ffc5b781a73d6413ef73fcba0ba311893
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2026-06-15 11:39:54 -0300

    feat: add reveal bands to settings scrollables (#24)

--- crates/momo-ui/src/components/quick_settings/main_menu/mod.rs
+++ crates/momo-ui/src/components/quick_settings/main_menu/mod.rs
@@ -7,7 +7,7 @@ use self::tile_grid::SettingsTileGrid;
 use self::top_row::SettingsTopRow;
 use super::common::{settings_middle_row, settings_row};
 use super::volume_control::VolumeControl;
-use crate::components::quick_settings::style::settings_content_container_style;
+use crate::components::quick_settings::style::{settings_content_container_style, SETTINGS_MENU_GAP, SETTINGS_TILE_HEIGHT};
 use daiko::widgets::scrollable::Scrollable;
 use daiko::{Element, component::Component, component::ComponentContext};
 
@@ -24,7 +24,8 @@ impl Component for MainMenu {
             .with_content(settings_middle_row(VolumeControl))
             .with_content(
                 Scrollable::new(SettingsTileGrid, "quick_settings_scrollable")
-                    .with_visible_scroll_bars(self.show_scroll_bars_when_overflowing),
+                    .with_visible_scroll_bars(self.show_scroll_bars_when_overflowing)
+                    .with_focus_reveal_band(0.0, SETTINGS_TILE_HEIGHT + SETTINGS_MENU_GAP * 2.0),
             )
     }
 }
